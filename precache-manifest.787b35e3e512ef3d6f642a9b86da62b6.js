f8c8f1cc78f210a8b207f7c6b37b5222c762a6f2
kalysh_maxym <503292@ukr.net>
2020-07-09 16:23:55 +0300

    Updates

---
+++ precache-manifest.787b35e3e512ef3d6f642a9b86da62b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8150a9970b26fcbd76c890a994a49738",
+    "url": "/starwars-finder/index.html"
+  },
+  {
+    "revision": "0b98dc8fe0df2a352f05",
+    "url": "/starwars-finder/static/css/main.ab646d28.chunk.css"
+  },
+  {
+    "revision": "eb4d1d53e62f9bb39302",
+    "url": "/starwars-finder/static/js/2.e7179aaa.chunk.js"
+  },
+  {
+    "revision": "0b98dc8fe0df2a352f05",
+    "url": "/starwars-finder/static/js/main.0a166453.chunk.js"
+  },
+  {
+    "revision": "16fe017c605dc076ad5e",
+    "url": "/starwars-finder/static/js/runtime-main.4ef2e4bf.js"
+  },
+  {
+    "revision": "0e88511488367041a1f3cc9f6c350174",
+    "url": "/starwars-finder/static/media/logo.0e885114.png"
+  },
+  {
+    "revision": "28a2950e890686d9db877549a53467b0",
+    "url": "/starwars-finder/static/media/sort-az.28a2950e.svg"
+  },
+  {
+    "revision": "a0382eab6c069de0ec1002fa39c4af1b",
+    "url": "/starwars-finder/static/media/sort-za.a0382eab.svg"
+  }
+]);
